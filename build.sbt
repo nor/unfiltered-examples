854831039ee976aa033cd74b05f4606e8adbdef3
Kristian Nordal <kristian.nordal@gmail.com>
2012-08-20 22:11:43 +0200

    o Initial

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "unfiltered-examples"
+
+version := "1-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  "net.databinder" %% "unfiltered-filter" % "0.6.4",
+  "net.databinder" %% "unfiltered-jetty" % "0.6.4"
+)
+
---
+++ src/main/scala/examples/Download.scala
@@ -0,0 +1,22 @@
+package examples
+
+import unfiltered.request.{Path, GET}
+import unfiltered.response.{ResponseHeader, ResponseStreamer}
+import java.io.OutputStream
+
+object Download extends App {
+
+  val download = unfiltered.filter.Planify {
+    case GET(Path("/download")) =>
+      new ResponseStreamer {
+        def stream(os: OutputStream) {
+          val is = getClass.getResourceAsStream("/download.this")
+          val buffer = new Array[Byte](1024 * 4)
+          Stream.continually(is.read(buffer)).takeWhile(-1 !=).foreach(_ => os.write(buffer))
+        }
+      } ~> ResponseHeader("Content-Disposition", List("attachment; filename=\"download.pdf\""))
+
+  }
+
+  unfiltered.jetty.Http(8080).plan(download).run()
+}
